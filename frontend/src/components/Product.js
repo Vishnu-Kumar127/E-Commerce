c23097b9acaa0503a286b089551220323eca9d5f
Vishnu Kumar <mjvishnukumar2003@gmail.com>
2024-10-07 00:52:33 +0530

    final commit

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+/* REACT-BOOTSTRAP */
+import { Card } from "react-bootstrap";
+
+/* REACT ROUTER */
+import { Link } from "react-router-dom";
+
+/* COMPONENTS */
+import Rating from "./Rating";
+
+function Product({ product }) {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+      <Card.Img 
+    src={product.image} 
+    style={{ 
+        width: '200px',  
+        height: '200px',  
+        objectFit: 'cover' 
+    }} 
+/>
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            color={"#f8e825"}
+          />
+        </Card.Text>
+
+        <Card.Text as="h3">₹{product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
